2941d305b506945857a0547cf8e1ec04f4328fa6
anfilat <anfilat@mail.ru>
2020-04-20 08:50:05 +0000

    Deploying to gh-pages from  @ d8e918b7b630d8f39854813cd9bcbf01ad3e96b0 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = 'cache-' + Date.now();
+const cacheName = 'cache-and-update-v1';
 const filesToCache = [
     './index.html',
     './global.css',
@@ -6,30 +6,51 @@ const filesToCache = [
     './build/bundle.js',
     './build/worker.js',
 ];
-self.addEventListener("install", function (event) {
+
+self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
-        })
+        caches
+            .open(cacheName)
+            .then(cache => cache.addAll(filesToCache))
     );
 });
-self.addEventListener("activate", function (event) {
+
+self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(thisCacheName) {
-                    if (thisCacheName !== cacheName) {
-                        return caches.delete(thisCacheName);
-                    }
-                })
-            );
-        })
+        caches
+            .keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames.map(thisCacheName => {
+                        if (thisCacheName !== cacheName) {
+                            return caches.delete(thisCacheName);
+                        }
+                    })
+                );
+            })
     );
 });
-self.addEventListener("fetch", function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+
+self.addEventListener("fetch", event => {
+    event.respondWith(fromCache(event.request));
+    event.waitUntil(update(event.request));
 });
+
+function fromCache(request) {
+    return caches
+        .open(cacheName)
+        .then(cache => {
+            return cache
+                .match(request)
+                .then(response => response || fetch(request));
+        });
+}
+
+function update(request) {
+    return caches
+        .open(cacheName)
+        .then(cache => {
+            return fetch(request)
+                .then(response => cache.put(request, response))
+        });
+}
